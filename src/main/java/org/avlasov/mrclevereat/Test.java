30c401b0d06a7f5faf80b266df27eb447caa483a
Vlasov Artem <avlasov@selectica.com>
2017-12-22 18:16:00 +0200

    Add base package structure, .gitignore

---
+++ src/main/java/org/avlasov/mrclevereat/Test.java
@@ -0,0 +1,7 @@
+package org.avlasov.mrclevereat;
+
+/**
+ * Created By artemvlasov on 22/12/2017
+ **/
+public class Test {
+}
---
+++ src/test/java/org/avlasov/mrclevereat/TestTest.java
@@ -0,0 +1,8 @@
+package org.avlasov.mrclevereat;
+
+/**
+ * Created By artemvlasov on 22/12/2017
+ **/
+class TestTest {
+
+}
