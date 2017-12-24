9f03cbbcb575afcdf82b74a67ea17f21fd2276e3
Vlasov Artem <avlasov@selectica.com>
2017-12-24 20:34:13 +0200

    Add CalculateProductData utils, that will caluclate NutritionalValue for Meal and Recipe. Add Test for new class

---
+++ src/main/java/org/avlasov/mrclevereat/utils/CalculateProductData.java
@@ -0,0 +1,145 @@
+package org.avlasov.mrclevereat.utils;
+
+import org.avlasov.mrclevereat.entity.product.MealProduct;
+import org.avlasov.mrclevereat.entity.ration.Meal;
+import org.avlasov.mrclevereat.entity.recipe.Recipe;
+import org.avlasov.mrclevereat.nutrition.NutritionalValue;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+/**
+ * Created By artemvlasov on 23/12/2017
+ **/
+public class CalculateProductData {
+
+    /**
+     * Calculate All required data from Meal, like {@link Meal#getRecipes()} and {@link Meal#getMealProducts()}
+     *
+     * @param meal Meal
+     */
+    public void calculateMealTotals(Meal meal) {
+        Objects.requireNonNull(meal);
+        NutritionalValueData nutritionalValueData = new NutritionalValueData();
+        calculateMealProducts(meal.getMealProducts())
+                .ifPresent(nvd -> mergeNutritionalValueData().apply(nutritionalValueData, nvd));
+        calculateRecipes(meal.getRecipes())
+                .ifPresent(nvd -> mergeNutritionalValueData().apply(nutritionalValueData, nvd));
+        meal.setVolume(nutritionalValueData.nutritionalVolume);
+        meal.setNutritionalValue(nutritionalValueData.nutritionalValue);
+    }
+
+    public void calculateRecipesTotals(Recipe recipe) {
+        Objects.requireNonNull(recipe);
+        Optional<NutritionalValueData> nutritionalValueData = calculateMealProducts(recipe.getMealProducts());
+        if (nutritionalValueData.isPresent()) {
+            recipe.setVolume(nutritionalValueData.get().nutritionalVolume);
+            recipe.setNutritionalValue(nutritionalValueData.get().nutritionalValue);
+        } else {
+            recipe.setNutritionalValue(new NutritionalValue());
+        }
+    }
+
+    private Optional<NutritionalValueData> calculateMealProducts(List<MealProduct> mealProducts) {
+        return Optional.ofNullable(mealProducts)
+                .orElse(Collections.emptyList())
+                .parallelStream()
+                .map(calculateMealProduct())
+                .reduce(mergeNutritionalValueData());
+    }
+
+    private Optional<NutritionalValueData> calculateRecipes(List<Recipe> recipes) {
+        return Optional.ofNullable(recipes)
+                .orElse(Collections.emptyList())
+                .parallelStream()
+                .peek(this::calculateRecipesTotals)
+                .map(recipe -> new NutritionalValueData(recipe.getNutritionalValue(), recipe.getVolume()))
+                .reduce(mergeNutritionalValueData());
+    }
+
+    private Function<MealProduct, NutritionalValueData> calculateMealProduct() {
+        return mp -> new NutritionalValueDataBuilder(mp.getVolume())
+                    .buildOnBaseNutritionalValue(mp.getProduct().getNutritionalValue());
+    }
+
+    private BinaryOperator<NutritionalValueData> mergeNutritionalValueData() {
+        return (identity, nvd) -> {
+            mergeNutritionalValues(identity.nutritionalValue, nvd.nutritionalValue);
+            identity.nutritionalVolume = identity.nutritionalVolume + nvd.nutritionalVolume;
+            return identity;
+        };
+    }
+
+    private void mergeNutritionalValues(NutritionalValue target, NutritionalValue source) {
+        target.setCalories(target.getCalories() + source.getCalories());
+        target.setCarbohydrate(target.getCarbohydrate() + source.getCarbohydrate());
+        target.setProtein(target.getProtein() + source.getProtein());
+        target.setFat(target.getFat() + source.getFat());
+    }
+
+    private class NutritionalValueData {
+        private NutritionalValue nutritionalValue;
+        private double nutritionalVolume;
+
+        NutritionalValueData(NutritionalValue nutritionalValue, double nutritionalVolume) {
+            this.nutritionalValue = nutritionalValue;
+            this.nutritionalVolume = nutritionalVolume;
+        }
+
+        NutritionalValueData() {
+            nutritionalValue = new NutritionalValue();
+        }
+    }
+
+    private class NutritionalValueDataBuilder {
+
+        private NutritionalValue nutritionalValue;
+        private double volume;
+        private double calculatedVolumePercent;
+
+        NutritionalValueDataBuilder(double volume) {
+            this.volume = volume;
+            this.nutritionalValue = new NutritionalValue();
+            calculatedVolumePercent = volume / 100;
+        }
+
+        NutritionalValueData buildOnBaseNutritionalValue(NutritionalValue baseNutritionalValue) {
+            return this
+                    .withProtein(baseNutritionalValue.getProtein())
+                    .withFat(baseNutritionalValue.getFat())
+                    .withCarbohydrate(baseNutritionalValue.getCarbohydrate())
+                    .withCalories(baseNutritionalValue.getCalories())
+                    .build();
+        }
+
+        NutritionalValueDataBuilder withProtein(double protein) {
+            nutritionalValue.setProtein(protein * calculatedVolumePercent);
+            return this;
+        }
+
+        NutritionalValueDataBuilder withFat(double fat) {
+            nutritionalValue.setFat(fat * calculatedVolumePercent);
+            return this;
+        }
+
+        NutritionalValueDataBuilder withCarbohydrate(double carbohydrate) {
+            nutritionalValue.setCarbohydrate(carbohydrate * calculatedVolumePercent);
+            return this;
+        }
+
+        NutritionalValueDataBuilder withCalories(double calories) {
+            nutritionalValue.setCalories(calories * calculatedVolumePercent);
+            return this;
+        }
+
+        NutritionalValueData build() {
+            return new NutritionalValueData(nutritionalValue, volume);
+        }
+
+    }
+
+}
---
+++ src/test/java/org/avlasov/mrclevereat/utils/CalculateProductDataTest.java
@@ -0,0 +1,124 @@
+package org.avlasov.mrclevereat.utils;
+
+import org.avlasov.mrclevereat.entity.product.MealProduct;
+import org.avlasov.mrclevereat.entity.product.Product;
+import org.avlasov.mrclevereat.entity.ration.Meal;
+import org.avlasov.mrclevereat.entity.recipe.Recipe;
+import org.avlasov.mrclevereat.nutrition.NutritionalValue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created By artemvlasov on 23/12/2017
+ **/
+class CalculateProductDataTest {
+
+    @Test
+    void calculateMealTotals_WithRequiredData_CalculateNutritionalValue() {
+        Meal meal = getMeal();
+        new CalculateProductData().calculateMealTotals(meal);
+        assertEquals(meal.getVolume(), 410);
+        assertEquals(meal.getNutritionalValue().getCalories(), 382);
+    }
+
+    @Test
+    void calculateMealTotals_WithProductsOnly_CalculateNutritionalValue() {
+        Meal meal = getMeal();
+        meal.setRecipes(null);
+        new CalculateProductData().calculateMealTotals(meal);
+        assertEquals(meal.getVolume(), 180);
+        assertEquals(meal.getNutritionalValue().getFat(), 4.73);
+    }
+
+    @Test
+    void calculateMealTotals_WithRecipesOnly_CalculateNutritionalValue() {
+        Meal meal = getMeal();
+        meal.setMealProducts(null);
+        new CalculateProductData().calculateMealTotals(meal);
+        assertEquals(meal.getVolume(), 230);
+        assertEquals(meal.getNutritionalValue().getCarbohydrate(), 9.129999999999999);
+    }
+
+    @Test
+    void calculateMealTotals_WithOutData_NoCalculation() {
+        Meal meal = getMeal();
+        meal.setRecipes(null);
+        meal.setMealProducts(null);
+        new CalculateProductData().calculateMealTotals(meal);
+        assertEquals(meal.getVolume(), 0);
+        assertEquals(meal.getNutritionalValue().getCarbohydrate(), 0);
+    }
+
+    @Test
+    void calculateMealTotals_WithNull_ThrownException() {
+        Assertions.assertThrows(NullPointerException.class, () -> new CalculateProductData().calculateMealTotals(null));
+    }
+
+    @Test
+    void calculateRecipesTotals_WithRequiredData_CalculateNutritionalValue() {
+        Recipe recipe = getRecipe();
+        new CalculateProductData().calculateRecipesTotals(recipe);
+        assertEquals(recipe.getVolume(), 230);
+        assertEquals(recipe.getNutritionalValue().getProtein(), 1.88);
+        assertEquals(recipe.getName(), "Vegetable Salad");
+    }
+
+    @Test
+    void calculateRecipesTotals_WithOutData_NoCalculation() {
+        Recipe recipe = getRecipe();
+        recipe.setMealProducts(null);
+        new CalculateProductData().calculateRecipesTotals(recipe);
+        assertEquals(recipe.getVolume(), 0);
+        assertEquals(recipe.getNutritionalValue().getProtein(), 0);
+    }
+
+    @Test
+    void calculateRecipesTotals_WithNull_ThrownException() {
+        Assertions.assertThrows(NullPointerException.class, () -> new CalculateProductData().calculateRecipesTotals(null));
+    }
+
+    private Meal getMeal() {
+        Meal meal = new Meal();
+        meal.setMealProducts(Arrays.asList(getMealProduct(130, getProduct("chicken breast", 31, 3.6, 0, 165)),
+                getMealProduct(50, getProduct("potato", 2, 0.1, 17,77))));
+        meal.setRecipes(Collections.singletonList(getRecipe()));
+        return meal;
+    }
+
+    private Recipe getRecipe() {
+        Recipe recipe = new Recipe();
+        recipe.setName("Vegetable Salad");
+        recipe.setMealProducts(Arrays.asList(
+                getMealProduct(50, getProduct("cucumber", 0.7, 0, 3.6, 16)),
+                getMealProduct(70, getProduct("tomato", 0.9, 0.2, 3.9, 18)),
+                getMealProduct(100, getProduct("bell pepper", 0.9, 0.2, 4.6, 20)),
+                getMealProduct(10, getProduct("olive oil", 0, 100, 0, 884))
+                ));
+        return recipe;
+    }
+
+    private MealProduct getMealProduct(double volume, Product product) {
+        MealProduct mealProduct = new MealProduct();
+        mealProduct.setVolume(volume);
+        mealProduct.setProduct(product);
+        return mealProduct;
+    }
+
+
+    private Product getProduct(String name, double protein, double fat, double carbohydrate, double calories) {
+        Product product = new Product();
+        product.setName(name);
+        NutritionalValue nutritionalValue = new NutritionalValue();
+        nutritionalValue.setProtein(protein);
+        nutritionalValue.setFat(fat);
+        nutritionalValue.setCarbohydrate(carbohydrate);
+        nutritionalValue.setCalories(calories);
+        product.setNutritionalValue(nutritionalValue);
+        return product;
+    }
+}
