d0d46b5f78da601ccc7de21a0c4377dcf3a9c53a
Vlasov Artem <avlasov@selectica.com>
2018-01-20 00:39:41 +0200

    Update MealRepository method with between date from LocalDate to LocalDateTime

--- src/main/java/org/avlasov/mrclevereat/repository/MealRepository.java
+++ src/main/java/org/avlasov/mrclevereat/repository/MealRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -14,7 +14,7 @@ import java.util.List;
 public interface MealRepository extends JpaRepository<Meal, Long> {
 
     Page<Meal> findByOwnerId(long id, Pageable pageable);
-    List<Meal> findByOwnerIdAndCreatedDate(long id, LocalDate localDate);
-    List<Meal> findByOwnerIdAndCreatedDateBetween(long id, LocalDate from, LocalDate to);
+    List<Meal> findByOwnerIdAndCreatedDate(long id, LocalDateTime createdDate);
+    List<Meal> findByOwnerIdAndCreatedDateBetween(long id, LocalDateTime from, LocalDateTime to);
 
 }
--- src/test/java/org/avlasov/mrclevereat/repository/MealRepositoryTest.java
+++ src/test/java/org/avlasov/mrclevereat/repository/MealRepositoryTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -35,13 +35,13 @@ class MealRepositoryTest extends RepositoryTestCase {
 
     @Test
     void findByOwnerIdAndCreatedDate() {
-        List<Meal> byOwnerIdAndCreatedDate = mealRepository.findByOwnerIdAndCreatedDate(1, LocalDate.of(2017, 12, 31));
+        List<Meal> byOwnerIdAndCreatedDate = mealRepository.findByOwnerIdAndCreatedDate(1, LocalDateTime.of(2017, 12, 31, 0, 0));
         assertThat(byOwnerIdAndCreatedDate, IsCollectionWithSize.hasSize(1));
     }
 
     @Test
     void findByOwnerIdAndCreatedDateBetween() {
-        List<Meal> byOwnerIdAndCreatedDateBetween = mealRepository.findByOwnerIdAndCreatedDateBetween(1, LocalDate.of(2017, 10, 1), LocalDate.of(2018, 1, 1));
+        List<Meal> byOwnerIdAndCreatedDateBetween = mealRepository.findByOwnerIdAndCreatedDateBetween(1, LocalDateTime.of(2017, 10, 1, 0, 0), LocalDateTime.of(2018, 1, 1, 0, 0));
         assertThat(byOwnerIdAndCreatedDateBetween, IsCollectionWithSize.hasSize(2));
     }
 }
