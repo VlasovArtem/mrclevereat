62a6abc6c9ad7cab89fbde6831f6c7b6a06be475
Vlasov Artem <avlasov@selectica.com>
2018-01-20 00:44:40 +0200

    Remove all data from App class

--- src/main/java/org/avlasov/mrclevereat/App.java
+++ src/main/java/org/avlasov/mrclevereat/App.java
@@ -1,11 +1,7 @@
 package org.avlasov.mrclevereat;
 
-import org.avlasov.mrclevereat.entity.user.User;
-import org.avlasov.mrclevereat.repository.UserRepository;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Created By artemvlasov on 26/12/2017
@@ -13,11 +9,5 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 @EnableAutoConfiguration
 public class App {
-
-    public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(App.class);
-        UserRepository bean = run.getBean(UserRepository.class);
-        bean.save(new User("email@mail.com", "hello".getBytes()));
-    }
 
 }
