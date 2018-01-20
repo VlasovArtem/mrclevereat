7f8487eea5113bf43ecec906f267aac4b9615311
Vlasov Artem <avlasov@selectica.com>
2018-01-20 15:27:37 +0200

    Add Component annotation for CalculateTargetWeight

--- src/main/java/org/avlasov/mrclevereat/utils/CalculateTargetWeight.java
+++ src/main/java/org/avlasov/mrclevereat/utils/CalculateTargetWeight.java
@@ -2,12 +2,14 @@ package org.avlasov.mrclevereat.utils;
 
 import org.avlasov.mrclevereat.entity.user.User;
 import org.avlasov.mrclevereat.entity.user.enums.Gender;
+import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 
 /**
  * Created By artemvlasov on 20/01/2018
  **/
+@Component
 public class CalculateTargetWeight {
 
     /**
