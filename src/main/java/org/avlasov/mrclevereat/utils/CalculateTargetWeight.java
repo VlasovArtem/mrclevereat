7870f56c95350b4e97737fdef3ca95e11b2b75e2
Vlasov Artem <avlasov@selectica.com>
2018-01-20 15:17:40 +0200

    Add utils class CalculateTargetWeight with Brokk Formula to calculate target weight, add test

---
+++ src/main/java/org/avlasov/mrclevereat/utils/CalculateTargetWeight.java
@@ -0,0 +1,31 @@
+package org.avlasov.mrclevereat.utils;
+
+import org.avlasov.mrclevereat.entity.user.User;
+import org.avlasov.mrclevereat.entity.user.enums.Gender;
+
+import java.util.Objects;
+
+/**
+ * Created By artemvlasov on 20/01/2018
+ **/
+public class CalculateTargetWeight {
+
+    /**
+     * Calculate Target Weight by Brokk Formula
+     *
+     * female: (height - 110) * 1,15
+     * male: (height - 100) * 1,15
+     *
+     * @param user user
+     * @return target weight
+     */
+    public double calculateTargetWeightByBrokkFormula(User user) {
+        Objects.requireNonNull(user);
+        int genderVariable = 100;
+        if (Gender.FEMALE.equals(user.getGender())) {
+            genderVariable = 110;
+        }
+        return (user.getHeight() - genderVariable) * 1.15;
+    }
+
+}
---
+++ src/test/java/org/avlasov/mrclevereat/utils/CalculateTargetWeightTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created By artemvlasov on 20/01/2018
+ **/
+class CalculateTargetWeightTest {
+
+}
