a4a3450e28b97ef9881ea12e420bbc73e6c33433
Vlasov Artem <avlasov@selectica.com>
2018-01-20 17:11:21 +0200

    Add new ResourceBunldeMessageResolver bean

--- src/main/java/org/avlasov/mrclevereat/config/AppConfig.java
+++ src/main/java/org/avlasov/mrclevereat/config/AppConfig.java
@@ -1,9 +1,12 @@
 package org.avlasov.mrclevereat.config;
 
 import org.avlasov.mrclevereat.config.database.DevDataSourceConfig;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 /**
  * Created By artemvlasov on 25/12/2017
@@ -12,4 +15,13 @@ import org.springframework.context.annotation.Import;
 @ComponentScan(basePackages = {"org.avlasov.mrclevereat.utils"})
 @Import(DevDataSourceConfig.class)
 public class AppConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource bundleMessageSource = new ResourceBundleMessageSource();
+        bundleMessageSource.setBasenames("validation/errormsg/diet_data");
+        bundleMessageSource.setDefaultEncoding("UTF-8");
+        return bundleMessageSource;
+    }
+
 }
